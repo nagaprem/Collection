a6741cb853e656ad5286ee783c79db93098850fe
nagaprem <nagaprem29@gmail.com>
2019-05-09 00:50:17 +0530

    merging

---
+++ src/com/GreensTech/ArrayList2.java
@@ -0,0 +1,19 @@
+package com.GreensTech;
+import java.util.*;  
+
+public class ArrayList2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> al=new ArrayList<String>();  
+		  al.add("Ravi");  
+		  al.add("Vijay");  
+		  al.add("Ravi");  
+		  al.add("Ajay");  
+		   //Traversing list through for-each loop  
+		  for(String obj:al)  
+		    System.out.println(obj);  
+
+	}
+
+}
